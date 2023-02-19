9923396eef079eddd1455e0ee6387d542f6e0e68
Bruno Figueiredo <brunonf15@gmail.com>
2023-02-19 18:49:58 +0000

    doing more test cases

--- features/form.feature
+++ features/form.feature
@@ -12,3 +12,32 @@ Feature: Submit a form
     When I enter incomplete form data
     And I click the Submit button
     Then I should assert that no have message
+  
+  Scenario: Verify that the First name field accepts only valid inputs
+    Given I am on the form page
+    When I enter an invalid First name
+    And I click the Submit button
+    Then I should assert that a valid First name is required
+    Then I should assert that no have message
+  
+  Scenario: Verify that the email field accepts only valid inputs
+    Given I am on the form page
+    When I enter an invalid email
+    And I click the Submit button
+    Then I should assert that a valid email is required
+    Then I should assert that no have message
+  
+  Scenario: Verify that the Phone number field accepts only valid inputs
+    Given I am on the form page
+    When I enter an invalid Phone number
+    And I click the Submit button
+    Then I should assert that a valid Phone number is required
+    Then I should assert that no have message
+
+  #This test have found a boud, it should be raised
+  #Scenario: Verify that the Phone number field accepts accpet properly valid inputs
+    #Given I am on the form page
+    #When I enter an valid Phone number
+    #And I click the Submit button
+    #And I click the Submit button
+    #Then I should assert the success message
--- features/step_definitions/form.steps.js
+++ features/step_definitions/form.steps.js
@@ -65,3 +65,95 @@ Then('I should assert that no have message', async () => {
   console.log('Form submitted successfully');
   await browser.close();
 });
+
+When('I enter an invalid First name', async () => {
+  const data = {
+    FirstName: '1',
+    LastName: 'Doe',
+    Email: 'johndoe@example.com',
+    PhoneNumber: '1234567890',
+    Gender: 'Male',
+    Agreement: true,
+  };
+  await page.fill('input[name="FirstName"]', data.FirstName);
+  await page.fill('input[name="LastName"]', data.LastName);
+  await page.fill('input[name="Email"]', data.Email);
+  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
+  await page.check(`input[type="radio"][value="${data.Gender}"]`);
+  await page.check('input[type="checkbox"][name="Agreement"]');
+});
+
+When('I should assert that a valid First name is required', async () => {
+  const errorMessage = 'Valid first name is required.';
+  const errorSelector = 'text=Valid first name is required.';
+  const errorElement = await page.waitForSelector(errorSelector);
+  const errorText = await errorElement.textContent();
+  expect(errorText).contain(errorMessage);
+});
+
+When('I enter an invalid Phone number', async () => {
+  const data = {
+    FirstName: 'bruno',
+    LastName: 'Doe',
+    Email: 'johndoe@example.com',
+    PhoneNumber: 'fff',
+    Gender: 'Male',
+    Agreement: true,
+  };
+  await page.fill('input[name="FirstName"]', data.FirstName);
+  await page.fill('input[name="LastName"]', data.LastName);
+  await page.fill('input[name="Email"]', data.Email);
+  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
+  await page.check(`input[type="radio"][value="${data.Gender}"]`);
+  await page.check('input[type="checkbox"][name="Agreement"]');
+});
+
+When('I enter an valid Phone number', async () => {
+  const data = {
+    FirstName: 'Bruno',
+    LastName: 'Doe',
+    Email: 'johndoe@example.com',
+    PhoneNumber: '+351915444876',
+    Gender: 'Male',
+    Agreement: true,
+  };
+  await page.fill('input[name="FirstName"]', data.FirstName);
+  await page.fill('input[name="LastName"]', data.LastName);
+  await page.fill('input[name="Email"]', data.Email);
+  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
+  await page.check(`input[type="radio"][value="${data.Gender}"]`);
+  await page.check('input[type="checkbox"][name="Agreement"]');
+});
+
+When('I should assert that a valid Phone number is required', async () => {
+  const errorMessage = 'Valid phone number is required.';
+  const errorSelector = 'text=Valid phone number is required.';
+  const errorElement = await page.waitForSelector(errorSelector);
+  const errorText = await errorElement.textContent();
+  expect(errorText).contain(errorMessage);
+});
+
+When('I enter an invalid email', async () => {
+  const data = {
+    FirstName: 'bruno',
+    LastName: 'Doe',
+    Email: 'testinvalid',
+    PhoneNumber: '129312931',
+    Gender: 'Male',
+    Agreement: true,
+  };
+  await page.fill('input[name="FirstName"]', data.FirstName);
+  await page.fill('input[name="LastName"]', data.LastName);
+  await page.fill('input[name="Email"]', data.Email);
+  await page.fill('input[name="PhoneNumber"]', data.PhoneNumber);
+  await page.check(`input[type="radio"][value="${data.Gender}"]`);
+  await page.check('input[type="checkbox"][name="Agreement"]');
+});
+
+When('I should assert that a valid email is required', async () => {
+  const errorMessage = 'Valid email is required.';
+  const errorSelector = 'text=Valid email is required.';
+  const errorElement = await page.waitForSelector(errorSelector);
+  const errorText = await errorElement.textContent();
+  expect(errorText).contain(errorMessage);
+});
